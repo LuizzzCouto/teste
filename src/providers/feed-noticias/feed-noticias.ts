02d205af0b709788c402bfb898cc8551c8df8f54
Luli <luizbreduardo@hotmail.com>
2018-12-14 22:17:57 -0200

    fedd

--- src/providers/feed-noticias/feed-noticias.ts
+++ src/providers/feed-noticias/feed-noticias.ts
@@ -1,15 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
 import { Noticia } from '../../models/noticia';
-=======
-import { Mensagem } from '../../models/mensagem';
->>>>>>> e26bd605655d4acccc8bbd88e21574931bebd33f
 
 
 @Injectable()
 export class FeedNoticiasProvider {
-<<<<<<< HEAD
 
   private noticias;
 
@@ -21,37 +16,8 @@ export class FeedNoticiasProvider {
     return this.http.get('https://caps-ad.herokuapp.com/public/feed/listar').toPromise().then(
       data=>{
         this.noticias = data;
-=======
-  private feednoticia;
-
-  constructor(public http: HttpClient) {
-    console.log('Hello UsuariosProvider Provider');
-  }
-
-  listar() {
-    return this.http.get('https://caps-ad.herokuapp.com/public/feed/listar').toPromise()
-    .then(
-      data=>{
-        this.feednoticia = data;
->>>>>>> e26bd605655d4acccc8bbd88e21574931bebd33f
         return data;
       }
     );
   }
-<<<<<<< HEAD
 }
-
-=======
-
-  listarPorId(id){
-    let feednoticia = this.feednoticia
-    .filter(
-      function(elemento) {
-        return elemento.id == id
-      }
-    )
-    return feednoticia[0];
-  }
-
-}
->>>>>>> e26bd605655d4acccc8bbd88e21574931bebd33f
